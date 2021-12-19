91879877502ac641fcc9a73d0a6241f793da58df
nurillahilwafi <nurilwafi17@gmail.com>
2021-12-19 16:11:56 +0700

    update wallet

--- Admin_samauntung/wallet.php
+++ Admin_samauntung/wallet.php
@@ -72,14 +72,6 @@ $a = query(" SELECT * FROM pesanan ");
               </div>
               
             </div>
-            <ol class="breadcrumb">
-                    <li>
-                    <a href="" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                      <i class="fas fa-download fa-sm text-white-50"></i> Input Transaction </a>
-                    <a href="" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                      <i class="fas fa-download fa-sm text-white-50"></i> Report </a>
-                    </li>
-                  </ol>
           </div>
           
           <div class="row">
