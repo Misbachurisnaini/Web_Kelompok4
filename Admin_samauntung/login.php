5d2efe1c0827021eb941d62b0fb485331320bea4
nurillahilwafi <nurilwafi17@gmail.com>
2021-12-21 14:27:25 +0700

    update login

--- Admin_samauntung/login.php
+++ Admin_samauntung/login.php
@@ -37,10 +37,12 @@ if (isset($_POST['login'])){
   <body class="bg-gradient-login">
     <!-- Login Content -->
     <div class="container-login" ng-app="myApp" ng-controller="myCon">
+    <center style="margin-top: 20px;margin-bottom:10px"><img src="img/logo/logo.png" width="150px" height="150px"></center>
       <div class="row justify-content-center">
         <div class="col-xl-6 col-lg-12 col-md-9">
           <div class="card shadow-sm my-5">
             <div class="card-body p-0">
+            
               <div class="row">
                 <div class="col-lg-12">
                   <div class="login-form">
